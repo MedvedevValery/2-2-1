b2d4ac844d555127ccf68ce4f6d98d69f3cbf566
MedvedevValery <sanasasa33@gmail.com>
2024-07-24 21:27:30 +0300

    221

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -13,18 +13,28 @@ public class User {
    @Column(name = "name")
    private String firstName;
 
-   @Column(name = "last_name")
+   @Column(name = "lastname")
    private String lastName;
 
    @Column(name = "email")
    private String email;
 
+   @OneToOne(cascade = CascadeType.ALL)
+   private Car car;
+
    public User() {}
-   
+
    public User(String firstName, String lastName, String email) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
+   }
+
+   public User(String firstName, String lastName, String email, Car car) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.email = email;
+      this.car = car;
    }
 
    public Long getId() {
@@ -57,5 +67,21 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+
+   public Car getCar(){
+      return car;
+   }
+
+   public void setCar(){
+      this.car = car;
+   }
+
+   @Override
+   public String toString() {
+      return "firstName: " + firstName + ", " +
+              "lastName: " + lastName + ", " +
+              "email: " + email + ", " +
+              car.toString();
    }
 }
